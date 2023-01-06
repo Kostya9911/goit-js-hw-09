66a10025d44652aab9ea7e4a43605a9073104d5a
Kostya9911 <koshka5557@i.ua>
2023-01-06 17:51:26 +0200

    tast 1 completed

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+startBtn.style.fontSize = '40px';
+stopBtn.style.fontSize = '40px';
+stopBtn.setAttribute('disabled', 'disabled');
+
+let timerId = null;
+
+startBtn.addEventListener('click', () => {
+  startBtn.setAttribute('disabled', 'disabled');
+  stopBtn.removeAttribute('disabled');
+
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+stopBtn.addEventListener('click', () => {
+  startBtn.removeAttribute('disabled');
+  stopBtn.setAttribute('disabled', 'disabled');
+  clearInterval(timerId);
+});
